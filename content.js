2e1814e07d1072cfa197450bc4d3dfe2f0ee7f61
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2021-01-17 16:11:44 +0530

    Added shapes and tools

--- content.js
+++ content.js
@@ -1,50 +1,78 @@
 console.log("chrome extension go? ");
 
 chrome.runtime.onMessage.addListener(gotMessage);
-let currColor="#000000"
-let currWeight=3;
-let currShape = "line";
+let currColor = "#000000"
+let currWeight = 3;
+let currShape = "pointShape";
+let currTool = "";
 function gotMessage(message, sender, sendResponse) {
-  console.log("inside got message",message.flag);
-  if(message.flag==0)
-    currColor=message.color;
-  else if(message.flag==1){
-    currWeight=3*message.weight;
-    console.log("currWeight: ",currWeight);
+  console.log("inside got message", message.flag);
+  if (message.flag == 0)
+    currColor = message.color;
+  else if (message.flag == 1) {
+    currWeight = 3 * message.weight;
+    console.log("currWeight: ", currWeight);
   }
-  // else if(message.flag==2){
-  //   currShape=message.shape;
-  //   console.log("shape: ",currShape);
-  // }
-    
+  else if (message.flag == 2) {
+    currShape = message.shape;
+    console.log("shape: ", currShape);
+  }
+  else if (message.flag == 3) {
+    currTool = message.tool;
+    console.log("tool: ", currTool);
+  }
 
 }
 // P5.js
-let s = (sketch)=>{
-  sketch.setup = ()=>{
-      //to not select the text while writing the content 
-      document.body.style['userSelect'] = "none"; 
-      let h=document.body.clientHeight;
-      let c=sketch.createCanvas(sketch.windowWidth,sketch.windowHeight);
-      c.position(0,0);
-      c.style('pinter-events','none');
-      sketch.clear();
+let s = (sketch) => {
+  sketch.setup = () => {
+    //to not select the text while writing the content 
+    document.body.style['userSelect'] = "none";
+    let h = document.body.clientHeight;
+    let c = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
+    c.position(0, 0);
+    c.style('pinter-events', 'none');
+    sketch.clear();
   };
 
-  sketch.draw=()=>{
-      sketch.stroke(currColor);
-      sketch.strokeWeight(currWeight);
-      if(sketch.mouseIsPressed){
-          //console.log("shape: ",shape);
-          sketch.line(sketch.mouseX,sketch.mouseY,sketch.pmouseX,sketch.pmouseY);
-          //rectangle
-          // sketch.rect(sketch.mouseX,sketch.mouseY,sketch.pmouseX,sketch.pmouseY);
-          // //ellipse
-          // sketch.ellipse(sketch.mouseX,sketch.mouseY,sketch.pmouseX,sketch.pmouseY);
-          // //point
-          // sketch.point(sketch.pmouseX,sketch.pmouseY);
+  sketch.draw = () => {
+    sketch.stroke(currColor);
+    sketch.strokeWeight(currWeight);
+    if (sketch.mouseIsPressed) {
+      //console.log("shape: ",shape);
+      if (currShape === "lineShape") {
+        sketch.line(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY);
       }
+      else if (currShape === "circleShape") {
+        sketch.ellipse(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY);
+      }
+      else if (currShape === "ellipseShape") {
+        sketch.ellipse(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY);
+      }
+      else if (currShape === "triangleShape") {
+        sketch.triangle(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY, sketch.pmouseX, sketch.pmouseY);
+      }
+      else if (currShape === "squareShape") {
+        sketch.rect(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY);
+      }
+      else {
+        sketch.point(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY);
+      }
+      //USING TOOLS
+      if (currTool === "bucketTool") {
+        sketch.fill(currColor);
+      }
+      // else if (currTool === "eraserTool") {
+      //   sketch.fill('white');
+      // }
+      // else if(currTool === "sprayTool"){
+      //   sketch.erase();
+      // }
+      // else if(currTool === "scissorsTool"){
+      //   sketch.erase();
+      // }
+    }
   };
 };
 
-let myp5= new p5(s);
+let myp5 = new p5(s);
--- sketch.js
+++ sketch.js
@@ -4,34 +4,39 @@ console.log("outside");
 function setup() {
   noCanvas();
   //COLORS
-  document.getElementById("redColor").addEventListener("click",()=>{changeColor('#FF0000')});
-  document.getElementById("orangeColor").addEventListener("click",()=>{changeColor('#F26202')});
-  document.getElementById("yellowColor").addEventListener("click",()=>{changeColor('#EAAE00')});
-  document.getElementById("greenColor").addEventListener("click",()=>{changeColor('#00FF00')});
-  document.getElementById("blueColor").addEventListener("click",()=>{changeColor('#0000FF')});
-  document.getElementById("whiteColor").addEventListener("click",()=>{changeColor('#FFFFFF')});
-  document.getElementById("blackColor").addEventListener("click",()=>{changeColor('#000000')});
-  document.getElementById("violetColor").addEventListener("click",()=>{changeColor('#5829BB')});
-  document.getElementById("pinkColor").addEventListener("click",()=>{changeColor('#E61A8D')});
+  document.getElementById("redColor").addEventListener("click", () => { changeColor('#FF0000') });
+  document.getElementById("orangeColor").addEventListener("click", () => { changeColor('#F26202') });
+  document.getElementById("yellowColor").addEventListener("click", () => { changeColor('#EAAE00') });
+  document.getElementById("greenColor").addEventListener("click", () => { changeColor('#00FF00') });
+  document.getElementById("blueColor").addEventListener("click", () => { changeColor('#0000FF') });
+  document.getElementById("whiteColor").addEventListener("click", () => { changeColor('#FFFFFF') });
+  document.getElementById("blackColor").addEventListener("click", () => { changeColor('#000000') });
+  document.getElementById("violetColor").addEventListener("click", () => { changeColor('#5829BB') });
+  document.getElementById("pinkColor").addEventListener("click", () => { changeColor('#E61A8D') });
 
   //STROKES
-  document.getElementById("1x").addEventListener("click",()=>{changeStroke(1)});
-  document.getElementById("2x").addEventListener("click",()=>{changeStroke(2)});
-  document.getElementById("3x").addEventListener("click",()=>{changeStroke(3)});
-  document.getElementById("4x").addEventListener("click",()=>{changeStroke(4)});
-  document.getElementById("5x").addEventListener("click",()=>{changeStroke(5)});
+  document.getElementById("1x").addEventListener("click", () => { changeStroke(1) });
+  document.getElementById("2x").addEventListener("click", () => { changeStroke(2) });
+  document.getElementById("3x").addEventListener("click", () => { changeStroke(3) });
+  document.getElementById("4x").addEventListener("click", () => { changeStroke(4) });
+  document.getElementById("5x").addEventListener("click", () => { changeStroke(5) });
 
   //SHAPES
-  // document.getElementById("point").addEventListener("click",()=>{changeShape("point")});
-  // document.getElementById("line").addEventListener("click",()=>{changeShape("line")});
-  // document.getElementById("circle").addEventListener("click",()=>{changeShape("circle")});
-  // document.getElementById("ellipse").addEventListener("click",()=>{changeShape("ellipse")});
-  // document.getElementById("square").addEventListener("click",()=>{changeShape("square")});
-  // document.getElementById("rectangle").addEventListener("click",()=>{changeShape("rectangle")});
+  document.getElementById("lineShape").addEventListener("click", () => { changeShape("lineShape") });
+  document.getElementById("circleShape").addEventListener("click", () => { changeShape("circleShape") });
+  document.getElementById("ellipseShape").addEventListener("click", () => { changeShape("ellipseShape") });
+  document.getElementById("squareShape").addEventListener("click", () => { changeShape("squareShape") });
+  document.getElementById("triangleShape").addEventListener("click", () => { changeShape("triangleShape") });
+
+  //TOOLS
+  document.getElementById("eraserTool").addEventListener("click", () => { changeTool("eraserTool") });
+  document.getElementById("bucketTool").addEventListener("click", () => { changeTool("bucketTool") });
+  document.getElementById("scissorsTool").addEventListener("click", () => { changeTool("scissorsTool") });
+  document.getElementById("sprayTool").addEventListener("click", () => { changeTool("sprayTool") });
 
 
-   //to change the color
-  function changeColor(currcolor){
+  //to change the color
+  function changeColor(currcolor) {
     let params = {
       active: true,
       currentWindow: true,
@@ -42,14 +47,14 @@ function setup() {
     function gotTabs(tabs) {
       let msg = {
         color: currcolor,
-        flag:0
+        flag: 0
       };
-      console.log("color",msg.color);
+      console.log("color", msg.color);
       chrome.tabs.sendMessage(tabs[0].id, msg);
     }
   };
   //to change the stroke
-  function changeStroke(currWeight){
+  function changeStroke(currWeight) {
     let params = {
       active: true,
       currentWindow: true,
@@ -60,34 +65,52 @@ function setup() {
     function gotTabs(tabs) {
       let msg = {
         weight: currWeight,
-        flag:1
+        flag: 1
       };
-      console.log("weight",msg.weight);
+      console.log("weight", msg.weight);
       chrome.tabs.sendMessage(tabs[0].id, msg);
     }
   }
 }
 
 
- //to change the shape
-//  function changeShape(currShape){
-//   let params = {
-//     active: true,
-//     currentWindow: true,
-//   };
+//to change the shape
+function changeShape(currShape) {
+  let params = {
+    active: true,
+    currentWindow: true,
+  };
 
-//   chrome.tabs.query(params, gotTabs);
+  chrome.tabs.query(params, gotTabs);
 
-//   function gotTabs(tabs) {
-//     let msg = {
-//       shape: currShape,
-//       flag:2
-//     };
-//     console.log("clicked shape",msg.shape);
-//     chrome.tabs.sendMessage(tabs[0].id, msg);
-//   }
-// };
+  function gotTabs(tabs) {
+    let msg = {
+      shape: currShape,
+      flag: 2
+    };
+    console.log("clicked shape", msg.shape);
+    chrome.tabs.sendMessage(tabs[0].id, msg);
+  }
+};
 
+//to change the tools
+function changeTool(currTool) {
+  let params = {
+    active: true,
+    currentWindow: true,
+  };
+
+  chrome.tabs.query(params, gotTabs);
+
+  function gotTabs(tabs) {
+    let msg = {
+      tool: currTool,
+      flag: 3
+    };
+    console.log("Tool selected", msg.tool);
+    chrome.tabs.sendMessage(tabs[0].id, msg);
+  }
+};
 
 
 
